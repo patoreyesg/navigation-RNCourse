59f38be783d3436180ddeaf1b7a7020271aeb043
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-19 21:42:10 -0400

    se añade el style en el texto

--- src/screens/Pagina3Screen.tsx
+++ src/screens/Pagina3Screen.tsx
@@ -1,13 +1,14 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {Button, Text, View} from 'react-native';
+import {styles} from '../theme/appTheme';
 
 interface Props extends StackScreenProps<any, any> {}
 
 export const Pagina3Screen = ({navigation}: Props) => {
   return (
-    <View>
-      <Text>Pagina 3</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Pagina 3</Text>
 
       <Button title="Regresar" onPress={() => navigation.pop()} />
       <Button title="Ir a Página 1" onPress={() => navigation.popToTop()} />
