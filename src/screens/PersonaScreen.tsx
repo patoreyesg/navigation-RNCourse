97f3f13f6b7f1869205fe482e12c22824acdb81d
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-19 21:43:42 -0400

    se crea nueva pantalla para probar el paso de argumentos e informacion

---
+++ src/screens/PersonaScreen.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {styles} from '../theme/appTheme';
+
+export const PersonaScreen = () => {
+  return (
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>PersonaScreen</Text>
+    </View>
+  );
+};
