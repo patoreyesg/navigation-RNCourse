50ed81f32a776e8272f348d5a9cff8130700a51f
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-19 20:49:54 -0400

    configuraciones para habilitar la navegacion

--- android/app/src/main/java/com/navegacionapp/MainActivity.java
+++ android/app/src/main/java/com/navegacionapp/MainActivity.java
@@ -1,4 +1,5 @@
 package com.navegacionapp;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -29,4 +30,11 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
+
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }  
+
 }
