adaa1e6fd3435477e9d0e5d4e0625adda8df2642
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-22 19:45:47 -0400

    habilitacion de mostrar el menu lateral siempre cuando se coloca de lado el dispositivo

--- src/navigator/MenuLateralBasico.tsx
+++ src/navigator/MenuLateralBasico.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {StackNavigator} from './StackNavigator';
 import SettingsScreen from '../screens/SettingsScreen';
+import {useWindowDimensions} from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico = () => {
+  const {width} = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerType: width >= 768 ? 'permanent' : 'front',
+      }}>
       <Drawer.Screen
         name="StackNavigator"
         options={{title: 'Home'}}
