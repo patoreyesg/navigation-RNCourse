39c437bda14ffe1c7bae5903c26a533804671c71
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-22 01:08:58 -0400

    creacion de screen para mostrar trabajo de parametros y argumentos enviados

--- src/screens/PersonaScreen.tsx
+++ src/screens/PersonaScreen.tsx
@@ -1,11 +1,24 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {styles} from '../theme/appTheme';
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParams} from '../navigator/StackNavigator';
 
-export const PersonaScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'PersonaScreen'> {}
+
+export const PersonaScreen = ({route, navigation}: Props) => {
+  const params = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: params.nombre,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <View style={styles.globalMargin}>
-      <Text style={styles.title}>PersonaScreen</Text>
+      <Text style={styles.title}>{JSON.stringify(params, null, 3)}</Text>
     </View>
   );
 };
