2f86314d02df6a7f32f9c1ac439803df306c7cda
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-22 01:09:23 -0400

    envio de parametros a pantalla de personas

--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,6 +1,6 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, TouchableOpacity, View} from 'react-native';
 import {styles} from '../theme/appTheme';
 
 interface Props extends StackScreenProps<any, any> {}
@@ -8,12 +8,36 @@ interface Props extends StackScreenProps<any, any> {}
 export const Pagina1Screen = ({navigation}: Props) => {
   return (
     <View style={styles.globalMargin}>
-      <Text style={styles.title}>Pagina 1</Text>
-
       <Button
         title="Ir a pagina 2"
         onPress={() => navigation.navigate('Pagina2')}
       />
+
+      <Text>Navegar con Argumentos</Text>
+
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity
+          style={styles.botonGrande}
+          onPress={() =>
+            navigation.navigate('PersonaScreen', {
+              id: 1,
+              nombre: 'Patricio Reyes',
+            })
+          }>
+          <Text style={styles.botonGrandeTexto}>Patricio</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.botonGrande}
+          onPress={() =>
+            navigation.navigate('PersonaScreen', {
+              id: 2,
+              nombre: 'Karen Berna',
+            })
+          }>
+          <Text style={styles.botonGrandeTexto}>Karen</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
