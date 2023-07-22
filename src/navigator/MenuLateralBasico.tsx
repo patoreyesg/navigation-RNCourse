fbcae8952633392765076b22840a6908d39e566f
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-22 01:18:10 -0400

    se agregan las pantallas del menu lateral

--- src/navigator/MenuLateralBasico.tsx
+++ src/navigator/MenuLateralBasico.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {StackNavigator} from './StackNavigator';
+import SettingsScreen from '../screens/SettingsScreen';
 
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico = () => {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Article" component={Article} />
+      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
     </Drawer.Navigator>
   );
 };
