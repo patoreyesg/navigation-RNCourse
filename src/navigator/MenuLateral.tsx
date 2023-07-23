03c9a2f2f1b5627421addd2f7a4ad4237ca687a1
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-22 20:33:55 -0400

    creacion de menu interno para el drawer

--- src/navigator/MenuLateral.tsx
+++ src/navigator/MenuLateral.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  createDrawerNavigator,
+} from '@react-navigation/drawer';
 import {StackNavigator} from './StackNavigator';
 import SettingsScreen from '../screens/SettingsScreen';
-import {useWindowDimensions} from 'react-native';
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+} from 'react-native';
+import {styles} from '../theme/appTheme';
 
 const Drawer = createDrawerNavigator();
 
@@ -13,9 +24,37 @@ export const MenuLateral = () => {
     <Drawer.Navigator
       screenOptions={{
         drawerType: width >= 768 ? 'permanent' : 'front',
-      }}>
+      }}
+      drawerContent={props => <MenuInterno {...props} />}>
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
       <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
     </Drawer.Navigator>
+  );
+};
+
+const MenuInterno = ({navigation}: DrawerContentComponentProps) => {
+  return (
+    <DrawerContentScrollView>
+      <View style={styles.avatarContainer}>
+        <Image
+          source={{
+            uri: 'https://er.isacs.eu/sites/default/files/flmngr/avatar-placeholder.png',
+          }}
+          style={styles.avatar}
+        />
+      </View>
+      <View style={styles.menuContainer}>
+        <TouchableOpacity
+          style={styles.menuBoton}
+          onPress={() => navigation.navigate('StackNavigator')}>
+          <Text style={styles.menuTexto}>Navegación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.menuBoton}
+          onPress={() => navigation.navigate('SettingsScreen')}>
+          <Text style={styles.menuTexto}>Ajustes</Text>
+        </TouchableOpacity>
+      </View>
+    </DrawerContentScrollView>
   );
 };
