1e40c1ad98a1631e556dd1797a8d8ec4524af0da
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-22 21:14:48 -0400

    actualizacion de pantalla de tabs para la navegacion entre las nuevas screen de tabs

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Tab1Screen from '../screens/Tab1Screen';
+import Tab2Screen from '../screens/Tab2Screen';
+import Tab3Screen from '../screens/Tab3Screen';
+
+const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
   return (
-    <View>
-      <Text>Tabs</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Tab2Screen"
+        options={{title: 'Tab 1'}}
+        component={Tab2Screen}
+      />
+      <Tab.Screen
+        name="Tab1Screen"
+        options={{title: 'Tab 2'}}
+        component={Tab1Screen}
+      />
+      <Tab.Screen
+        name="Tab3Screen"
+        options={{title: 'Tab 3'}}
+        component={Tab3Screen}
+      />
+    </Tab.Navigator>
   );
 };
