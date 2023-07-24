8d5a4027d675d45d4dca1fd20b42ad9d52ab5b70
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-24 01:06:17 -0400

    se crea componente para usar el contexto en App

--- App.tsx
+++ App.tsx
@@ -4,15 +4,22 @@ import {NavigationContainer} from '@react-navigation/native';
 // import {StackNavigator} from './src/navigator/StackNavigator';
 // import {MenuLateralBasico} from './src/navigator/MenuLateralBasico';
 import {MenuLateral} from './src/navigator/MenuLateral';
+import {AuthProvider} from './src/context/AuthContext';
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/* <StackNavigator /> */}
-      {/* <MenuLateralBasico /> */}
-      <MenuLateral />
+      <AppState>
+        {/* <StackNavigator /> */}
+        {/* <MenuLateralBasico /> */}
+        <MenuLateral />
+      </AppState>
     </NavigationContainer>
   );
+};
+
+const AppState = ({children}: any) => {
+  return <AuthProvider>{children}</AuthProvider>;
 };
 
 export default App;
