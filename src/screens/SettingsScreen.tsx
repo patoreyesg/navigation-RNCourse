9c9f0c98006b3095a3b3d6ff0741870e1899ac31
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-24 01:07:38 -0400

    se prueba el uso del contexto mostrando la informacion

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,10 +1,12 @@
+import React, {useContext} from 'react';
 import {View, Text} from 'react-native';
-import React from 'react';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {styles} from '../theme/appTheme';
+import {AuthContext} from '../context/AuthContext';
 
 export default function SettingsScreen() {
   const insets = useSafeAreaInsets();
+  const {authState} = useContext(AuthContext);
   return (
     <View
       style={{
@@ -12,6 +14,8 @@ export default function SettingsScreen() {
         marginTop: insets.top,
       }}>
       <Text style={styles.title}>SettingsScreen</Text>
+
+      <Text style={styles.title}>{JSON.stringify(authState, null, 4)}</Text>
     </View>
   );
 }
