f61fadc0073c5106100a317061113b30b4bd267e
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-24 00:05:04 -0400

    se agrega estilo de colores para ver texto

--- src/screens/Tab2Screen.tsx
+++ src/screens/Tab2Screen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {colores} from '../theme/appTheme';
 export default function Tab2Screen() {
   return (
     <View>
-      <Text>Tab2Screen</Text>
+      <Text style={{color: colores.textoGeneral}}>Tab2Screen</Text>
     </View>
   );
 }
